7687a7f096c5e1d1cdd1cae08f315540f4319c58
Yan <yan.yuhtman@gmail.com>
2025-12-23 11:05:07 +0200

    Initial Commit

---
+++ DexTests/DexTests.swift
@@ -0,0 +1,17 @@
+//
+//  DexTests.swift
+//  DexTests
+//
+//  Created by Yan  on 23/12/2025.
+//
+
+import Testing
+@testable import Dex
+
+struct DexTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
